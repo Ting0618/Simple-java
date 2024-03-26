08757a07c727f38b1a5e89aad17f9a031d6f457d
githubting <ting@1141169705@qq.com>
2024-03-26 14:57:15 +0800

    Link finished

--- src/Algorithm/mylink/MyLink.java
+++ src/Algorithm/mylink/MyLink.java
@@ -12,6 +12,11 @@ public class MyLink {
         head = new LinkNode();
     }
 
+    public MyLink(LinkNode head, int size) {
+        this.head = head;
+        this.size = size;
+    }
+
     /** Given the head of a linked list and an integer val, remove all the nodes of the linked list that has
      * Node.val == val, and return the new head.*/
     public LinkNode removeNodes(LinkNode head, int val){
@@ -133,10 +138,134 @@ public class MyLink {
         pre.next = temp.next;
         size--;
     }
+    
+    /**
+     * create by: Ting
+     * description: TODO reverse the list
+     * create time: 19/03/2024 19:57
+     */
+    public void reverse(){
+        LinkNode pre = null;
+        LinkNode cur = head;
+        LinkNode temp;
+        while (cur != null){
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        head = pre;
+    }
+
+
+    /**
+     * create by: Ting
+     * description: TODO Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+     * create time: 19/03/2024 20:56
+     */
+    public void swapPairs(){
+
+
+    }
+
+
+    /**
+     * create by: Ting
+     * description: TODO remove the nth node from the end of the list and return its head.
+     * create time: 25/03/2024 14:30
+     */
+    public void removeNthFromEnd(int n){
+        LinkNode fast = head;
+        LinkNode slow = head;
+        if (n <= 0 ){
+            return;
+        }
+        //find fast
+        for (int i = 0; i < n; i++) {
+            if(fast.next != null){
+                fast = fast.next;
+            } else {
+                return;
+            }
+        }
+        while (fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        // detele
+        LinkNode temp = slow.next;
+        slow.next = temp.next;
+    }
+
+
+    /**
+     * create by: Ting
+     * description: TODO Given two (singly) linked lists, determine if the two lists intersect. Return the inter secting node
+     * create time: 25/03/2024 16:55
+     */
+    public LinkNode getIntersectionNode(MyLink linkA, MyLink linkB){
+        LinkNode indexA = linkA.head;
+        LinkNode indexB = linkB.head;
+        int sizeA = linkA.getSize();
+        int sizeB = linkB.getSize();
+        int n = sizeB - sizeA;;
+        if(n >= 0){
+            // to find the location of indexB
+            for (int i = 0; i < n; i++) {
+                if(indexB.next != null){
+                    indexB = indexB.next;
+                }else
+                    return null;
+            }
+        } else {
+            // to find the location of indexA
+            for (int i = 0; i < -n; i++) {
+                if(indexA.next != null){
+                    indexA = indexA.next;
+                }else
+                    return null;
+            }
+        }
+        // indexA and indexB move together
+        while (indexA != null || indexB != null){
+            if (indexA == indexB){  //when they are equal
+                return indexA;
+            }else {
+                if(indexA != null)
+                    indexA = indexA.next;
+                if(indexB != null)
+                    indexB = indexB.next;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * create by: Ting
+     * description: TODO 判断一个链表有没有环
+     * create time: 25/03/2024 21:18
+     */
+    public boolean isCycle(){
+        LinkNode slow = head;
+        LinkNode fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
 
     public int getSize(){
         return this.size;
     }
+
+    public void setHead(LinkNode head) {
+        this.head = head;
+    }
+
     @Override
     public String toString() {
         return "MyLink{" +
--- src/Algorithm/mylink/Test.java
+++ src/Algorithm/mylink/Test.java
@@ -7,30 +7,39 @@ package Algorithm.mylink;
  */
 public class Test {
     public static void main(String[] args){
-//        LinkNode n6 = new LinkNode(0,null);
-//        LinkNode n5 = new LinkNode(1,n6);
-//        LinkNode n4 = new LinkNode(2,n5);
-//        LinkNode n3 = new LinkNode(3,n4);
-//        LinkNode n2 = new LinkNode(4,n3);
-//        LinkNode n1 = new LinkNode(5,n2);
-//        LinkNode head = new LinkNode(6,n1);
-        MyLink myLink = new MyLink();
-        myLink.addAtIndex(0,5);
-        myLink.addAtIndex(1,9);
-        myLink.addAtIndex(1,8);
-        int size = myLink.getSize();
-        System.out.println(size);
+        LinkNode n6 = new LinkNode(6,null);
+        LinkNode n5 = new LinkNode(5,n6);
+        LinkNode n4 = new LinkNode(4,n5);
+        LinkNode n3 = new LinkNode(3,n4);
+        LinkNode n2 = new LinkNode(2,n3);
+        LinkNode n1 = new LinkNode(1,n2);
+        LinkNode headA = new LinkNode(0,n1);
+
+        LinkNode m2 = new LinkNode(22,null);
+        LinkNode m1 = new LinkNode(11,m2);
+        LinkNode headB = new LinkNode(0,m1);
+
+        MyLink myLink = new MyLink(headA,6);
+//        myLink.setHead(headA);
+
+        MyLink myLinkB = new MyLink(headB,5);
+//        myLinkB.setHead(headB);
+//        myLink.addAtIndex(0,4);
+//        myLink.addAtIndex(1,2);
+//        myLink.addAtIndex(2,3);
+
+
         System.out.println(myLink);
-        myLink.deleteAtIndex(1);
-        System.out.println(myLink.getSize());
+//        myLink.removeNthFromEnd(1);
+//        myLink.swapPairs();
 //        LinkNode res = myLink.removeNodes(head,3);
 //        int res2 = myLink.getIndexth(head,1);
 //        myLink.addAtHead(9);
 //        myLink.addAtHead(8);
-//        myLink.addAtTail(0);
-//        myLink.addAtTail(5);
-//        myLink.addAtTail(9);
 
-        System.out.println(myLink);
+
+        System.out.println(myLinkB);
+        LinkNode temp = myLink.getIntersectionNode(myLink,myLinkB);
+        System.out.println(temp);
     }
 }
