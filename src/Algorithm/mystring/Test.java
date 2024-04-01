a4206738271795e3842e176bebc8c5c1d9b39a21
ting <tingwong0704@gmail.com>
2024-04-01 13:42:34 +0800

    make it public

--- src/Algorithm/mystring/Test.java
+++ src/Algorithm/mystring/Test.java
@@ -10,8 +10,8 @@ public class Test {
         String s = "hello";
         MyString myString = new MyString();
         String res = myString.reverseStr("abcd",4);
-//        res = myString.replaceNumber("a1b2c3");
-//        res = myString.reverseWords("  hello   world  ");
+        res = myString.replaceNumber("a1b2c3");
+        res = myString.reverseWords("  hello   world  ");
         res = myString.specialReverse("abcdefg",2);
         System.out.println(res);
     }
