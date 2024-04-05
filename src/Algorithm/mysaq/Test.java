01e4cc39deff50d2847214aa2d13b996eb0131cf
ting <tingwong0704@gmail.com>
2024-04-05 20:22:53 +0800

    implement Queue using Stack

---
+++ src/Algorithm/mysaq/Test.java
@@ -0,0 +1,9 @@
+package Algorithm.mysaq;
+
+public class Test {
+    public static void main(String[] args) {
+        Mystack mystack = new Mystack();
+        boolean res = mystack.isValid("]");
+        System.out.println(res);
+    }
+}
